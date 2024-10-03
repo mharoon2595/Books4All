503582860adec82d82b7a7a3232841a106d338fc
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-10-03 10:08:17 +0530

    final push

--- db/firebase.js
+++ db/firebase.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBiAA7k5dQPDpyN-JfSEp9q4cvqx2L08TU",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "books4everyone-56eea.firebaseapp.com",
   projectId: "books4everyone-56eea",
   storageBucket: "books4everyone-56eea.appspot.com",
